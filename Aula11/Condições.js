939dfa4edcd8743177c976e4ccf3afc5d395348c
Jorge Lucas <si.jorgelucas@gmail.com>
2021-01-11 16:34:31 -0300

    js/html

---
+++ Aula11/Condições.js
@@ -0,0 +1,5 @@
+// console.log('O console funcionou corretamente')
+var vel = 60.5
+console.log(`A velocidade do seu carro é: ${vel}Km/h`)
+if (vel > 60) console.log("Você ultrapassou o limite de velocidade.")
+console.log("Dirija com cautela")
